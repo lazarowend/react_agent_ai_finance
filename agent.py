fd5ae536817f0cd3cb2dbfacb33129251c3c6bc8
lazarowend <lazaroclans1@gmail.com>
2024-12-12 15:33:39 -0300

    add agent executor

--- agent.py
+++ agent.py
@@ -1,12 +1,11 @@
 import os
 from dotenv import load_dotenv
 from langchain import hub
-from langchain.agents import Tool
+from langchain.agents import Tool, create_react_agent, AgentExecutor
 from langchain.prompts import PromptTemplate
 from langchain_community.tools import DuckDuckGoSearchRun
 from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain_experimental.utilities import PythonREPL
-from langchain_experimental.agents.agent_toolkits import create_python_agent
 
 
 load_dotenv()
@@ -30,13 +29,36 @@ python_repl = PythonREPL()
 python_repl_tool = Tool(
     name='Python REPL',
     description='Um shell python. Use isso para executar códigos python. Execute apenas código Python seguros. Se você precisar obter o retorno do código, use a função "print()". Use para realizar cálculos financeiros necessários para responder as perguntas',
-    func=python_repl
+    func=python_repl.run
 )
 
 search = DuckDuckGoSearchRun()
 duckduckgo_tool = Tool(
     name='DuckDuckGo',
     description='Útil para encontra informações e dicas de enconomia e opções de investimento. Você sempre deve pesquisar na internet para obter as melhores dicas usando esta ferramenta, Responda diretamente. Sua resposta deve informar que há elementos pesquisados na internet',
-    func=search
+    func=search.run
 )
 
+react_instructions = hub.pull('hwchase17/react')
+
+tools = [python_repl_tool, duckduckgo_tool]
+
+agent = create_react_agent(
+    llm=model,
+    tools=tools,
+    prompt=react_instructions
+)
+
+agent_executor = AgentExecutor(
+    agent=agent,
+    tools=tools,
+    verbose=True
+)
+
+question = 'vale a pena investir na empresa ISRG na bolsa americana? me de inshigs, faça a analise financeira para mim'
+
+output = agent_executor.invoke(
+    {'input': prompt_template.format(q=question)}
+)
+
+print(output.get('output'))
